894c7eec55c2a798697efd66e16de1a5887dfb2a
thudthudtonk <47860822+thudthudtonk@users.noreply.github.com>
2024-01-13 19:48:22 -0600

    scripting1
    first external scripting, also adding some images to another assets folder

---
+++ scriptTest.js
@@ -0,0 +1,16 @@
+var count = 0;
+function incrementCounter() {
+	count += 1;
+	document.getElementById("counter").innerHTML = count.toString();
+}
+
+function doAlert() {
+	alert("hello");
+}
+
+var countButton = document.getElementById("counterButton");
+counterButton.addEventListener("click", incrementCounter);
+
+
+var testButton = document.getElementById("alertButton");
+alertButton.addEventListener("click", doAlert);
