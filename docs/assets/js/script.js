870505d7c1bbb898348647d6cd4478fb46bda760
thudthudtonk <47860822+thudthudtonk@users.noreply.github.com>
2024-01-15 09:54:29 -0600

    playin around
    made a little button clicking coin flipping game just to do more stuff with JavaScript and stuff

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -4,6 +4,33 @@ function incrementCounter() {
 	document.getElementById("counter").innerHTML = count.toString();
 }
 
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max);
+}
+
+function redBlueFlip() {
+	var test1 = getRandomInt(2);
+	if (test1 == 0) {
+		document.getElementById("colorParagraph").innerHTML = "red";
+		return;
+	} else {
+		document.getElementById("colorParagraph").innerHTML = "blue";
+		return;
+	}
+}
+
+function redBlueCheck(color) {
+	if (color == document.getElementById("colorParagraph").innerHTML) {
+		document.getElementById("rightWrong").innerHTML = "correct";
+		incrementCounter();
+		redBlueFlip();
+		return;
+	} else {
+		document.getElementById("rightWrong").innerHTML = "incorrect";
+		return;
+	}
+}
+
 function doAlert() {
 	alert("hello");
 }
